0f830457b350a2032ef87e5f1a663a24924fd11b
Nabin Thomas <nabin@youknowwhere>
2019-06-11 16:54:47 -0700

    Movies screen 1st take. Has dummy content and some text there now.

--- app/src/main/java/com/nabin/starwarshub/MoviesActivity.java
+++ app/src/main/java/com/nabin/starwarshub/MoviesActivity.java
@@ -8,8 +8,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+
+import com.nabin.starwarshub.dummy.DummyContent;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.movie_list_view);
+        recyclerView.setHasFixedSize(true);
+
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new MovieListAdapter(DummyContent.ITEMS, new MovieFragment.OnListFragmentInteractionListener() {
+            @Override
+            public void onListFragmentInteraction(DummyContent.DummyItem item) {
+                System.out.println("onListFragmentInteraction  on " +  item.toString());
+            }
+        });
+        recyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -50,3 +79,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
